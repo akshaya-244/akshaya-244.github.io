52b28882d6cbd0834f30a3bf60b77d64dc6836ca
Akshaya Mohan <akshayamohan.2401@gmail.com>
2024-08-14 08:34:22 -0700

    Debug part 1

---
+++ myportfolio/components/Navbar.tsx
@@ -0,0 +1,77 @@
+"use client"
+import { useState } from "react";
+import { FileText, Github, House, Linkedin, LinkedinIcon, Mail, Menu } from "lucide-react";
+import Link from "next/link";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="bg-black text-white">
+      <div className="flex py-6 justify-between items-center mx-16">
+        <div className="ml-6">
+          <Link href="/"><House /></Link>
+        </div>
+        <div className="hidden md:flex w-1/4 justify-between">
+          <div>
+            <Link href="/about">About</Link>
+          </div>
+          <div>
+            <Link href="/projects">Projects</Link>
+          </div>
+          <div>
+            <Link href="/techstack">Tech Stack</Link>
+          </div>
+          <Link href="https://github.com/akshaya-244">
+            <Github />
+          </Link>
+          <Link href="https://www.linkedin.com/in/24-akshaya-mohan/">
+            <LinkedinIcon />
+          </Link>
+          <Link href="https://drive.google.com/file/d/1z68TaOC7lz4ml897Ibt76gkJp6_7XeHR/view?usp=sharing">
+            <FileText />
+          </Link>
+          <Link href="">
+            <Mail />
+          </Link>
+        </div>
+        <div className="md:hidden mr-6">
+          <button onClick={toggleMenu}>
+            <Menu />
+          </button>
+        </div>
+      </div>
+      {isOpen && (
+        <div className="md:hidden min-w-full flex flex-col justify-evenly items-center bg-black text-white py-4">
+         
+          <Link href="/about" onClick={toggleMenu}>
+            About
+          </Link>
+          <Link href="/projects" onClick={toggleMenu}>
+            Projects
+          </Link>
+          <Link href="/techstack" onClick={toggleMenu}>
+            Tech Stack
+          </Link>
+          <div className="min-w-full mt-4 flex justify-evenly">
+          <Link href="https://github.com/akshaya-244" onClick={toggleMenu}>
+            <Github />
+          </Link>
+          <Link href="https://www.linkedin.com/in/24-akshaya-mohan/" onClick={toggleMenu}>
+            <LinkedinIcon />
+          </Link>
+          <Link href="" onClick={toggleMenu}>
+            <Mail />
+          </Link>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
